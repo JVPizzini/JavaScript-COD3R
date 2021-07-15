616034420ca9119a8d7c8de955b8f83a44367427
JVPizzini <joaovitorpizzini@hotmail.com>
2021-07-14 23:25:54 -0300

    commit

--- Node/funcionarios/funcionarios.js
+++ Node/funcionarios/funcionarios.js
@@ -9,13 +9,21 @@ const menorSalario = (func,funcAtual)=>{
 
  axios.get(url).then(response => {
     const funcionarios = response.data
-    //   console.log(funcionarios)
+    // console.log(funcionarios)
 
-//    Mulher chinesa com o menor salário?
-    const func = funcionarios
-    .filter(chineses)
-    .filter(mulheres)
-    .reduce(menorSalario)
+    // Mulher chinesa com o menor salário?
+    const func = funcionarios.filter(chineses).filter(mulheres).reduce(menorSalario)
 
     console.log(func)
+})
+
+axios.get(url).then(resp =>{
+    const funcionarios = resp.data
+    
+    const searchBrazil = f => f.pais === 'Brazil'
+
+    const brasileiros = funcionarios.filter(searchBrazil).reduce(menorSalario)
+
+    console.log(brasileiros)
+
 })
