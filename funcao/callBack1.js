fa3afc53dc75a3b1a123b128c458d172035d5476
JVPizzini <joaovitorpizzini@hotmail.com>
2021-07-17 14:34:43 -0300

    commit

--- funcao/callBack1.js
+++ funcao/callBack1.js
@@ -1,10 +1,23 @@
 const fabrincante = ["Mercedes", "Audi", "BMW"]
 
-function imprimir(nome, indice)
+function imprimir(nome, teste)
 {
-    console.log(`${indice + 1}. ${nome}`)
+    console.log(`${teste}. ${nome}`)
 }
 
 fabrincante.forEach(imprimir)
 fabrincante.forEach(fabricante => console.log(fabricante))  
 
+
+function teste2(a,b,c,d){
+    console.log(`${b} - ${a} - ${c} - ${d}`)
+    console.log(typeof(a))
+    console.log(typeof(b))
+    console.log(typeof(c))
+    console.log(typeof(d))
+    
+}
+
+const teste = ["oi","Tchau","oie"]
+
+teste.forEach(teste2)
